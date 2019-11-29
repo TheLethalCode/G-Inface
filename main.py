c28fabd5d79cbffa1b04eecf26b8eae1dd7fce61
thelethalcode <kousshikraj.raj@gmail.com>
2019-11-29 09:22:02 +0530

    Added necessary functions - untested

--- main.py
+++ main.py
@@ -1,76 +1,45 @@
 from authenticate import authorize
 from data import data
+import sync
 import os, sys
 
-# def find_root(service):
-    
-#     page_token = None
+def syncAll(data, service):
 
-#     while True:
-#         results = service.files().list(
-#                 q = "'root' in parents",
-#                 fields = "nextPageToken, files(id, name, mimeType, ownedByMe, parents, size)",
-#                 pageToken = page_token
-#             ).execute()
-        
-#         for file in results.get('files',[]):
-#             print(file['name'])
+    """
+    Syncs the local folders specified to the drive
+    """
 
-#         page_token = results.get('nextPageToken',None)
-#         if page_token is None:
-#             break
+    for folder in data.foldersUpload:
+        sync.syncFolder(folder, service, data.folderId)
 
-def createSync(service):
 
-    page_token = None
-    hostname = os.uname()[1]
+def addFolders(data, folders):
+    """
+    Adds the given folders to the list of contents to sync
+    """
+    pass
 
-    while True:
 
-        # Look for all folders in the root directory
-        results = service.files().list(
-                q = "'root' in parents and mimeType = 'application/vnd.google-apps.folder'",
-                fields = "nextPageToken, files(id, name)",
-                pageToken = page_token
-            ).execute()
-        
-        # If required folder is found, return its ID
-        for file in results.get('files',[]):
-            if "G_{}".format(hostname) == file['name']:
-                return file['id']
-
-        page_token = results.get('nextPageToken',None)
-        if page_token is None:
-            break
-
-    # Else create the folder
-    metadata = {
-        'name' : 'G_{}'.format(hostname),
-        'mimeType' : 'application/vnd.google-apps.folder'
-    }
-
-    fileData = service.files().create(
-                    body = metadata,
-                    fields = 'id'
-                    ).execute()
-
-    # And return its id
-    return fileData['id']
+def removeFolders(data, folders):
+    """
+    Removes the folders from the list of contents to be synced
+    """
+    pass
 
 
 def main():
     
+    # Initialize the data and service objects
     curData = data()
     service = authorize()
 
     # If the file doesn't exists
     if not curData.exists:
         
-        # Get the ID of the sync folder in the drive
-        id = createSync(service)
-
-        # Create the file with the required data
+        # Get the ID of the sync folder in the drive and create the json file
+        id = sync.createSync(service)
         curData.createFile(os.uname()[1],id)
+    
     
 
 if __name__ == "__main__":
---
+++ sync.py
@@ -0,0 +1,141 @@
+import os
+
+def uploadFile(filePath, service, par_id):
+    """
+    Uploads the content of a file to the given directory id.
+    """
+
+    # Uploads the file under the directory pointed by par_if
+    metadata = {
+        'name' : os.path.basename(filePath),
+        'parents' : [par_id]
+    }
+
+    status = service.files().create(body = metadata, media_body = filePath, fields = 'id')
+
+    # Check the status of uploading
+    if status.get('id',None) is None:
+        print("Error Uploading {} ......".format(os.path.basename(filePath)))
+        exit(0)
+
+    else:
+        print("Success Uploading {} ......".format(os.path.basename(filePath)))
+
+
+def removeFile(service, id):
+    """
+    Remove the file with the given id
+    """
+
+    # Delete the file given by its is
+    service.files().delete(id)
+
+
+def syncFolder(syncPath, service, par_id):
+    """
+    Syncs a particular local folder under the specified parent directoty.
+    If the local file doesn't already exist, it uploads the contents of the file.
+    """
+    page_token = None
+    
+    while True:
+    
+        # Look for all folders in the parent directory
+        results = service.files().list(
+                q = "'{}' in parents".format(par_id),
+                fields = "nextPageToken, files(id, name, size)",
+                pageToken = page_token
+            ).execute()
+        
+        # If the required folder to be synced is found, check if all
+        # the contents of the directory are present in the drive
+
+        for file in results.get('files',[]):
+            if os.path.basename(syncPath) == file['name']:
+
+                # After finding the item, check whether it is a directory or file
+                # If directory, recurse and sync all of its contents
+                if os.path.isdir(syncPath):
+                    for fileFolder in os.listdir(syncPath):
+                        syncFolder(fileFolder,service, file['id'])
+
+                # If file, check whether the contents are the same
+                else:
+
+                    # If they are not the same, remove the old and upload the new
+                    if not ( abs( os.stat(syncPath).st_size - file['size'] ) < 100 ) : 
+                        removeFile(service, file['id'])
+                        uploadFile(syncPath, service, par_id)
+
+                return
+
+        page_token = results.get('nextPageToken',None)
+        if page_token is None:
+            break
+    
+    # If the directory is not present, create it.
+    if os.path.isdir(syncPath):
+
+        # Creating the directory under the parent directory
+        metadata = {
+            'name' : os.path.basename(syncFolder),
+            'mimeType' : 'application/vnd.google-apps.folder',
+            'parents'  : [par_id]
+        }
+
+        # Getting the ID of the created directory
+        fileData = service.files().create(
+                        body = metadata,
+                        fields = 'id'
+                        ).execute()
+
+        # Syncing the subfolders
+        for fileFolder in os.listdir(syncPath):
+            syncFolder(fileFolder,service, fileData['id'])
+
+    # If it is a file, upload it
+    else:
+        uploadFile(syncPath, service, par_id)
+
+
+def createSync(service):
+
+    """
+    Looks for the local computer's sync folder in the drive. If it is present, it returns its id.
+    If not, it creates the folder and returns the id.
+    """
+
+    page_token = None
+    hostname = os.uname()[1]
+
+    while True:
+
+        # Look for all folders in the root directory
+        results = service.files().list(
+                q = "'root' in parents and mimeType = 'application/vnd.google-apps.folder'",
+                fields = "nextPageToken, files(id, name)",
+                pageToken = page_token
+            ).execute()
+        
+        # If required folder is found, return its ID
+        for file in results.get('files',[]):
+            if "G_{}".format(hostname) == file['name']:
+                return file['id']
+
+        page_token = results.get('nextPageToken',None)
+        if page_token is None:
+            break
+
+    # Else create the folder
+    metadata = {
+        'name' : 'G_{}'.format(hostname),
+        'mimeType' : 'application/vnd.google-apps.folder'
+    }
+
+    fileData = service.files().create(
+                    body = metadata,
+                    fields = 'id'
+                    ).execute()
+
+    # And return its id
+    return fileData['id']
